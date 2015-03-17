e38ae2d6fd863d04599938fea4d6ec2d5fc375fa
Tanishqi <skyb3x@gmail.com>
2015-03-16 20:45:32 -0400

    Update newcitizens.user.js

--- newcitizens.user.js
+++ newcitizens.user.js
@@ -1,1 +1,10 @@
+// ==UserScript==
+// @name        New Citizens
+// @namespace   https://github.com/Tanishqi/vPopulus/blob/master/newcitizens.user.js
+// @description Gets a list of new citizens for a particular country in vPopulus
+// @include     about:blank
+// @exclude     %exclude%
+// @version     1
+// @grant       none
+// ==/UserScript==
 
