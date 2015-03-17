453ce8563d2dc83ca6e1f55c303ddcd171043e5a
Tanishqi <skyb3x@gmail.com>
2015-03-17 00:00:34 -0400

    Update newcitizens.user.js

--- newcitizens.user.js
+++ newcitizens.user.js
@@ -2,34 +2,15 @@
 // @name        New Citizens
 // @namespace   https://github.com/Tanishqi/vPopulus/blob/master/newcitizens.user.js
 // @description Gets a list of new citizens for a particular country in vPopulus
-// @include     about:blank
+// @include     http://api.vpopulus.net/
 // @version     1
 // @grant       none
 // ==/UserScript==
 
-<script>
-var url = "http://api.vpopulus.net/v1/feeds/country/citizens.json?id=4";
-var getUrl = function(url) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open ("GET", url, true);
-  xmlHttp.onload = function(e) {
-    if (xmlHttp.readyState === 4) {
-      if (xmlHttp.status === 200) {
-        console.log(xmlHttp.responseText);
-      } else {
-        console.error(xmlHttp.statusText);
-      }
-    }
-  };
-  xmlHttp.onerror = function(e) {
-    console.error(xmlHttp.statusText);
-  };
-  xmlHttp.send (null);
-  return xmlHttp.responseText;
-}
+var url = "http://www.api.vpopulus.net/v1/feeds/country/citizens.xml?id=4";
+var xmlHttp = new GM_XMLHttpRequest();
+xmlHttp.open ("GET", url, true);
+xmlHttp.send ();
+var xmlDoc = xmlhttp.responseXML;
 
-console.log(getUrl());
-
-document.write(getURL().citizens.citizen[1].id);
-
-</script>
+document.getElementById("top").innerHTML = "Hello, world!";
