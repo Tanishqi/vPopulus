7f0177c200da234fdf869b903fd7e02b32ab7025
Tanishqi <skyb3x@gmail.com>
2015-05-09 13:22:19 -0400

    Update newcitizens.user.js

--- newcitizens.user.js
+++ newcitizens.user.js
@@ -1,16 +1,31 @@
 // ==UserScript==
-// @name        New Citizens
+// @name        NewCitizens
 // @namespace   https://github.com/Tanishqi/vPopulus/blob/master/newcitizens.user.js
 // @description Gets a list of new citizens for a particular country in vPopulus
-// @include     http://api.vpopulus.net/
+// @include     http://vpoptools.altervista.org/
 // @version     1
-// @grant       none
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
-var url = "http://www.api.vpopulus.net/v1/feeds/country/citizens.xml?id=4";
-var xmlHttp = new GM_XMLHttpRequest();
-xmlHttp.open ("GET", url, true);
-xmlHttp.send ();
-var xmlDoc = xmlhttp.responseXML;
+GM_xmlhttpRequest({
+  method: "GET",
+  url: "https://www.vpopulus.net/api/citizen/getByID/8358",
+  onload: function(response) {
+    var responseXML = null;
+    // Inject responseXML into existing Object (only appropriate for XML content).
+    if (!response.responseXML) {
+      responseXML = new DOMParser()
+        .parseFromString(response.responseText, "text/xml");
+    }
 
-document.getElementById("top").innerHTML = "Hello, world!";
+    GM_log([
+      response.status,
+      response.statusText,
+      response.readyState,
+      response.responseHeaders,
+      response.responseText,
+      response.finalUrl,
+      responseXML
+    ].join("\n"));
+  }
+});
