432ad0d3f13f2bede26336141041546dd40e876e
Tanishqi <skyb3x@gmail.com>
2015-05-09 20:04:11 -0400

    Update newcitizens.user.js

--- newcitizens.user.js
+++ newcitizens.user.js
@@ -5,18 +5,55 @@
 // @include     http://vpoptools.altervista.org/
 // @version     1
 // @grant       GM_xmlhttpRequest
-// @grant       GM_log
 // ==/UserScript==
 
-GM_xmlhttpRequest({
-  method: "GET",
-  url: "https://www.vpopulus.net/api/citizen/getByID/8358",
-  onload: function(response) {
-    var responseXML = null;
-    // Inject responseXML into existing Object (only appropriate for XML content).
-    if (!response.responseXML) {
-      responseXML = new DOMParser()
-        .parseFromString(response.responseText, "text/xml");
+function findNumber() {
+  GM_xmlhttpRequest({
+    method: "GET",
+    url: "http://api.vpopulus.net/v1/feeds/country/citizens.xml?id=2",
+    onload: function(response) {
+      var responseXML = null;
+      var N = response.responseXML;
+      number = N.getElementsByTagName("pages")[0].childNodes[0].nodeValue;
+      console.log(number);
     }
+  });
+}
+
+function callPage() {
+  GM_xmlhttpRequest({
+    method: "GET",
+    url: "http://api.vpopulus.net/v1/feeds/country/citizens.xml?id=2&page=" + integer + "",
+    onload: function(response) {
+      var responseXML = null;
+      var R = response.responseXML;
+      console.log(R);
+
+      var citizens = R.getElementsByTagName("citizen");
+      console.log(citizens);
+    
+      var i = 0;
+      while (citizens[i]) {
+        if (i == null) { break; }
+        console.log(citizens[i]);
+        var entry1 = citizens[i].childNodes[3].childNodes[0].nodeValue;
+        var newEntry = document.createElement("DIV");
+        var textEntry = document.createTextNode(entry1);
+        newEntry.appendChild(textEntry);
+        document.getElementById("data").appendChild(newEntry);
+        i++;
+      }
+    }
+  });
+}
+
+function callPages () {
+  findNumber();
+  var integer = 1;
+  while (integer < number) {
+    callPage();
+    integer++;
   }
-});
+}
+
+callPages();
